41e57aa6f7a24c1355ef3c5ea0c6ad73c69b8d6f
Md Shuhan Miah <shuhan747@gmail.com>
2024-04-25 14:51:34 +0100

    initial commit

---
+++ New_Project_Solution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace New_Project_Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
